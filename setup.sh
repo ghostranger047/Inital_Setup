e4480ea36b94717c24c85d5a5f2dba026343de39
ghostRanger047 <niladrisarkar066@gmail.com>
2020-11-14 14:05:54 +0530

    Fixed new line display

--- setup.sh
+++ setup.sh
@@ -6,14 +6,14 @@ common()
 	printf "alias python=python3\n">>"$HOME/.bashrc"
 
 	##Download/install XDM
-	echo -n "\e[1;31m Downloading XDM.....\e[0m"
+	echo -n "\n\n\n\n\e[1;31m Downloading XDM.....\e[0m\n"
 	wget -O "xdm-setup-7.2.11.tar.xz" "https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz"
 	tar xvf xdm-setup-7.2.11.tar.xz
 	echo -n "Installing XDM"
 	sudo ./install.sh
 
 	##Java 15 setup
-	printf "\e[1;31m Downloading OpenJDK 15....\e[0m"
+	printf "\n\n\n\n\e[1;31m Downloading OpenJDK 15....\e[0m\n"
 	wget -O "openjdk-15.0.1_linux-x64_bin.tar.gz" "https://download.java.net/java/GA/jdk15.0.1/51f4f36ad4ef43e39d0dfdbaf6549e32/9/GPL/openjdk-15.0.1_linux-x64_bin.tar.gz"
 	sudo tar xvf openjdk-15.0.1_linux-x64_bin.tar.gz -C /opt
 	printf "export JAVA_HOME=/opt/jdk-15.0.1\n">>"$HOME/.bashrc"
@@ -24,17 +24,22 @@ common()
 
 ubuntu()
 {
+	##Sys upgrade
+	printf "\e[1;31m Performing system upgrade\e[0m\n"
+	sudo apt update
+	sudo apt -y upgrade
+	
 	##Brave installation
-	echo -n "\e[1;31m Installing  Brave Browser Stable.....\e[0m"
+	printf "\n\n\n\n\e[1;31m Installing  Brave Browser Stable.....\e[0m\n"
 	sudo apt install apt-transport-https curl gnupg
 	curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 	echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 	sudo apt update
-	sudo apt install -y brave-browser
+	sudo apt -y install brave-browser
 
 	##Required
-	echo -n "\e[1;31m Installing build tools....\e0m"
-	sudo apt install -y build-essential git wget vim qbittorrent
+	printf "\n\n\n\n\e[1;31m Installing build tools....\e0m\n"
+	sudo apt -y install build-essential git wget vim qbittorrent
 
 
 }
