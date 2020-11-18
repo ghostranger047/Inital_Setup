afd0bc2a23819b5d318298bc4affa6dd09ead64c
ghostRanger047 <niladrisarkar066@gmail.com>
2020-11-18 09:24:53 +0530

    More Fixes

--- setup.sh
+++ setup.sh
@@ -6,18 +6,20 @@ common()
 	printf "alias python=python3\n">>"$HOME/.bashrc"
 
 	##Download/install XDM
-	echo -n "\n\n\n\n\e[1;31m Downloading XDM.....\e[0m\n"
+	printf "\n\n\n\n\e[1;31m Downloading XDM.....\e[0m:\n"
 	wget -O "xdm-setup-7.2.11.tar.xz" "https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz"
 	tar xvf xdm-setup-7.2.11.tar.xz
 	echo -n "Installing XDM"
 	sudo ./install.sh
+	printf "\e[1;42mDone\e[0m"
 
 	##Java 15 setup
-	printf "\n\n\n\n\e[1;31m Downloading OpenJDK 15....\e[0m\n"
+	printf "\n\n\n\n\e[1;31m Downloading OpenJDK 15....\e[0m:\n"
 	wget -O "openjdk-15.0.1_linux-x64_bin.tar.gz" "https://download.java.net/java/GA/jdk15.0.1/51f4f36ad4ef43e39d0dfdbaf6549e32/9/GPL/openjdk-15.0.1_linux-x64_bin.tar.gz"
 	sudo tar xvf openjdk-15.0.1_linux-x64_bin.tar.gz -C /opt
 	printf "export JAVA_HOME=/opt/jdk-15.0.1\n">>"$HOME/.bashrc"
 	printf 'export PATH=${PATH}:${JAVA_HOME}/bin\n'>>"$HOME/.bashrc"
+	printf "\e[1;42mDone\e[0m"
 	
 	
 }
@@ -25,34 +27,33 @@ common()
 ubuntu()
 {
 	##Sys upgrade
-	printf "\e[1;31m Performing system upgrade\e[0m\n"
+	printf "\e[1;31m Performing system upgrade\e[0m:\n"
 	sudo apt -y update > /dev/null 2>&1
 	#sudo apt -y upgrade > /dev/null 2>&1
-	printf "Done"
+	printf "\e[1;42mDone\e[0m"
 	
 	##Brave installation
-	printf "\n\n\n\n\e[1;31m Installing  Brave Browser Stable.....\e[0m\n"
+	printf "\n\n\n\n\e[1;31m Installing  Brave Browser Stable.....\e[0m:\n"
 	sudo apt -y install apt-transport-https curl gnupg > /dev/null 2>&1
 	curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | sudo apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
 	echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main" | sudo tee /etc/apt/sources.list.d/brave-browser-release.list
 	sudo apt -y update > /dev/null 2>&1
 	sudo apt -y install brave-browser > /dev/null 2>&1
-	printf "Done"
+	printf "\e[1;42mDone\e[0m"
 
 	##Required
-	printf "\n\n\n\n\e[1;31m Installing build and tools....\e[0m\n"
+	printf "\n\n\n\n\e[1;31m Installing build and tools....\e[0m:\n"
 	sudo apt -y install build-essential git wget vim > /dev/null 2>&1
-	printf "Done"
+	printf "\e[1;42mDone\e[0m"
 
-	##Utility
-	sudo apt -y revome vlc firefox
-	sudo apt install mpv
+	##Utility Removal/installation:
+	sudo apt -y revome firefox
 
 
-	##
-	printf "\n\n\n\n\e[1;31mCleaning installation\e[0m"
-	sudo apt -y autoremove /dev/null 2>&1
-	printf "Done"
+	##Autoremove
+	printf "\n\n\n\n\e[1;31mCleaning installation\e[0m:\n"
+	sudo apt -y autoremove > /dev/null 2>&1
+	printf "\e[1;42mDone.\e[0m"
 
 }
 
